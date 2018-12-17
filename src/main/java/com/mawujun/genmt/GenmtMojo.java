e182f07f0b23f71190002b4d00e1f0211bff74e3
mawujun <mawujun@aikucun.com>
2018-12-17 20:51:23 +0800

    初始化

---
+++ src/main/java/com/mawujun/genmt/GenmtMojo.java
@@ -0,0 +1,49 @@
+package com.mawujun.genmt;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+/**
+ * 使用方法，添加插件：
+ * M和T的自动生成插件：
+<plugins>
+			<plugin>
+				<groupId>com.mawujun</groupId>
+				<artifactId>gen-maven-plugin</artifactId>
+				<version>1.0</version>
+				<executions>
+					<execution>
+						<phase>compile</phase>
+						<goals>
+							<goal>genmt</goal>
+						</goals>
+					</execution>					 
+				</executions>
+			</plugin>
+		</plugins>
+
+ * @author admin
+ *
+ */
+@Mojo( name = "genmt",defaultPhase=LifecyclePhase.COMPILE)
+public class GenmtMojo extends AbstractMojo {
+
+	@Override
+	public void execute() throws MojoExecutionException, MojoFailureException {
+		// TODO Auto-generated method stub
+		 getLog().info( "Hello, world." );
+		 getLog().info("================================");
+		 getLog().info("==========================================");
+			getLog().info("==========================================");
+			getLog().info("==========================================");
+			getLog().info("==========================================");
+			getLog().info("==========================================");
+			getLog().info("==========================================");
+			getLog().info("==========================================");
+			getLog().info("==========================================");
+
+	}
+
+}
